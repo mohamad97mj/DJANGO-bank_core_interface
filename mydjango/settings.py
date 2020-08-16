5bbbe9b48a81fcc4582c4c5ee7ec27c81910f998
mohamad <mohamad97mj@gmail.com>
2020-08-16 17:12:11 +0430

    change db port and username

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'PASSWORD': 'mJ604998',
         'HOST': 'localhost',
         # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': 5433,  # Set to empty string for default.
+        'PORT': 6000,  # Set to empty string for default.
     }
 
     # 'default': {
