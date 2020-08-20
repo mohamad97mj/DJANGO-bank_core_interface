a3453d657ee5137c600018f18cfbb7ece0699550
mohamad <mohamad97mj@gmail.com>
2020-08-20 18:34:52 +0430

    drop sql updated

--- myapp/sql_scripts/drop_data_base.sql
+++ myapp/sql_scripts/drop_data_base.sql
@@ -1,36 +1,32 @@
-drop table if exists django_migrations;
+drop table if exists django_migrations cascade;
 
-drop table if exists auth_group_pearmissions;
+drop table if exists auth_group_permissions cascade;
 
-drop table if exists myapp_userprofile_owners;
+drop table if exists myapp_userprofile_owners cascade;
 
-drop table if exists myapp_transaction;
+drop table if exists myapp_transaction cascade;
 
-drop table if exists myapp_userprofile;
+drop table if exists myapp_userprofile cascade;
 
-drop table if exists myapp_subcontract;
+drop table if exists myapp_subcontract cascade;
 
-drop table if exists myapp_normalcontract;
+drop table if exists myapp_normalcontract cascade;
 
-drop table if exists myapp_judgeprofile;
+drop table if exists myapp_judgeprofile cascade;
 
-drop table if exists myapp_owner;
+drop table if exists myapp_owner cascade;
 
-drop table if exists myapp_authprofile_groups;
+drop table if exists myapp_authprofile_groups cascade;
 
-drop table if exists auth_group;
+drop table if exists auth_group cascade;
 
-drop table if exists myapp_authprofile_user_permissions;
+drop table if exists myapp_authprofile_user_permissions cascade;
 
-drop table if exists auth_permission;
+drop table if exists auth_permission cascade;
 
-drop table if exists django_admin_log;
+drop table if exists django_admin_log cascade;
 
-drop table if exists django_content_type;
+drop table if exists django_content_type cascade;
 
-drop table if exists authtoken_token;
-
-drop table if exists myapp_authprofile;
-
-drop table if exists django_session;
+drop table if exists myapp_authprofile cascade;
 
