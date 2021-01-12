83556056ac8cca89851a3fa5cfe7b07560d4e658
mohamad <mohamad97mj@gmail.com>
2021-01-12 16:10:00 +0330

    drop db updated

--- myapp/sql_scripts/drop_data_base.sql
+++ myapp/sql_scripts/drop_data_base.sql
@@ -2,19 +2,19 @@ drop table if exists django_migrations cascade;
 
 drop table if exists auth_group_permissions cascade;
 
-drop table if exists myapp_userprofile_owners cascade;
+drop table if exists myapp_judgeprofile cascade;
+
+drop table if exists myapp_normalcontract cascade;
+
+drop table if exists myapp_owner cascade;
+
+drop table if exists myapp_reporterprofile cascade;
+
+drop table if exists myapp_subcontract cascade;
 
 drop table if exists myapp_transaction cascade;
 
 drop table if exists myapp_userprofile cascade;
-
-drop table if exists myapp_subcontract cascade;
-
-drop table if exists myapp_normalcontract cascade;
-
-drop table if exists myapp_judgeprofile cascade;
-
-drop table if exists myapp_owner cascade;
 
 drop table if exists myapp_authprofile_groups cascade;
 
@@ -28,5 +28,9 @@ drop table if exists django_admin_log cascade;
 
 drop table if exists django_content_type cascade;
 
+drop table if exists authtoken_token cascade;
+
 drop table if exists myapp_authprofile cascade;
+
+drop table if exists django_session cascade;
 
