34da5ed256ab149ff26f55e2a8961dfae2f93942
mohamad <mohamad97mj@gmail.com>
2021-05-20 10:37:06 +0430

    commit all before migration

--- myapp/views/views_logout.py
+++ myapp/views/views_logout.py
@@ -7,7 +7,6 @@ class LogoutView(APIView):
 
     def get(self, request):
 
-
         if role in LOGIN_ROLES:
             response = redirect(reverse('myapp:login'))
         elif role in SPECIAL_LOGIN_ROLES:
